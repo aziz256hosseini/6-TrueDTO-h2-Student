7992cd3d6eee21068d5d81d2f74ba2c6a7d32dde
shosseini <mohajer13sam@gmail.com>
2022-08-29 15:01:04 +0430

    TimeStamp

--- src/main/java/org/example/TestDatabaseDTOconnection/dto/StudentDTO.java
+++ src/main/java/org/example/TestDatabaseDTOconnection/dto/StudentDTO.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Column;
 import java.util.Date;
 
 @Data
